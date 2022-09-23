3f1ad67e4313f595be8ad6557c3e548ee121ff83
Tyler M <t_mckenzie@outlook.com>
2022-09-23 17:32:54 -0500

    Little practice assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'To Do List';
+
+  list: any[]=[];
+
+  addTask(item: string) {
+      this.list.push({id:this.list.length, name: item})
+  }
+
+  removeTask(id:number) {
+    this.list=this.list.filter(item => item.id!==id)
+  }
 }
